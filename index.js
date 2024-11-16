2919a545325f91dc32c77aec0bcfe2b5315ed57f
Benjamin <bmd7700@users.noreply.github.com>
2024-11-15 19:26:45 -0500

    add readme

--- index.js
+++ index.js
@@ -11,40 +11,14 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "Here, we help you decide what to do in Lakeland.";
 
+//buttons: random experience, food, parks, activities, shopping
+
+//each button on click should select a random event from the appropriate category
+
+//each category contains an array of endpoints. For now, just strings. Phase II: make it return objects with phone, address, etc etc. 
+
+
+
 container.appendChild(pageHeader);
 container.appendChild(content);
 
-
-
-/**const container = document.querySelector("#container");
-
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the GLORIOUS text content!";
-
-const paragraph = document.createElement("p");
-paragraph.textContent = "Hey I'm Red!";
-paragraph.style.color = "red";
-
-const blueHeader = document.createElement("h3");
-blueHeader.style.color = "blue";
-blueHeader.textContent = "I'm a blue H3!";
-
-const divContainer = document.createElement("div");
-divContainer.setAttribute("id", "newContainer");
-divContainer.style.border = "1px solid black";
-divContainer.style.backgroundColor = "pink";
-
-const newContainerHeader = document.createElement("h1");
-newContainerHeader.textContent = "I'm in a div!";
-
-const newParagraph = document.createElement("p");
-newParagraph.textContent = "ME TOO!";
-
-divContainer.appendChild(newContainerHeader);
-divContainer.appendChild(newParagraph);
-
-container.appendChild(content);
-container.appendChild(paragraph);
-container.appendChild(blueHeader);
-container.appendChild(divContainer); */
---
+++ readme.md
@@ -0,0 +1,13 @@
+The Lakeland Thing-Doer is a page designed to help people find an activity to do in Lakeland.
+
+It is a simple page with a banner bar and five buttons.
+
+1: Random Experience. Selects a random experience from a random category.
+2: Food.
+3: Parks.
+4: Activities.
+5: Shopping.
+
+On click, each button should alert the user to the winning selection.
+
+Eventually, we can have the buttons open a page with detailed information about each option.
