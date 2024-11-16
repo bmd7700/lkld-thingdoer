fdb86d2636d7d455e8a99a068e2fffa0690b6ab6
Benjamin <bmd7700@users.noreply.github.com>
2024-11-15 22:57:59 -0500

    add all buttons, begin click logic

--- index.js
+++ index.js
@@ -15,21 +15,88 @@ content.textContent = "Helping YOU decide what to do in Lakeland!";
 //buttons: random experience, food, parks, activities, shopping
 //
 
-const buttons = document.createElement("div");
-buttons.classList.add("buttons");
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("buttons");
 
 //each button on click should select a random event from the appropriate category
 const randomButton = document.createElement("button");
 randomButton.classList.add("buttons");
+randomButton.setAttribute("id", "randomButton");
 randomButton.textContent = "RANDOM CHOICE";
+
+const foodButton = document.createElement("button");
+foodButton.classList.add("buttons");
+foodButton.setAttribute("id", "foodButton");
+foodButton.textContent = "FOODS";
+
+const parksButton = document.createElement("button");
+parksButton.classList.add("buttons");
+parksButton.setAttribute("id", "parksButton");
+parksButton.textContent = "PARKS";
+
+const activitiesButton = document.createElement("button");
+activitiesButton.classList.add("buttons");
+activitiesButton.setAttribute("id", "activitiesButton");
+activitiesButton.textContent = "ACTIVITIES";
+
+const shoppingButton = document.createElement("button");
+shoppingButton.classList.add("buttons");
+shoppingButton.setAttribute("id", "shoppingButton");
+shoppingButton.textContent = "SHOPPING";
 
 //each category contains an array of endpoints. For now, just strings. Phase II: make it return objects with phone, address, etc etc. 
 
-// use the appendChild method to add children to a div
+// when each button is clicked, a new div should populate with the selected item
+const resultDiv = document.createElement("div");
+resultDiv.classList.add("content");
 
+let placeholder = "";
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", ()=>{
+        let selection = button.id;
+
+    })
+})
+
+function selectList(selection){
+    if(selection === "randomButton"){
+
+    } else {
+        
+    }
+    //pull random number from random function (create function)
+    //assign number to index in array named after button id
+}
+
+const resultContent = document.createElement("p");
+resultContent.innerText = placeholder;
+
+
+resultDiv.appendChild(resultContent);
 
 banner.appendChild(pageHeader);
-buttons.appendChild(randomButton);
+buttonContainer.appendChild(randomButton);
+buttonContainer.appendChild(foodButton);
+buttonContainer.appendChild(parksButton);
+buttonContainer.appendChild(activitiesButton);
+buttonContainer.appendChild(shoppingButton);
 container.appendChild(content);
-container.appendChild(buttons);
+container.appendChild(buttonContainer);
+container.appendChild(resultDiv);
 
+/**
+ * e references the event and unlocks a number of properties and methods we can use to parse the event
+const callback = document.querySelector("#callback");
+callback.addEventListener("click", function(e){
+    console.log(e.target.style.background = "blue");
+});
+
+//using the querySelectorAll tool we can apply code to all elements of the selected type
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", ()=> {
+        alert(button.id);
+    });
+}); */
